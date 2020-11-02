f262a7801d399179e556f98f457f755a4148e8bf
Fran Dios <frankdiox@gmail.com>
2020-11-02 15:48:21 +0900

    Redirect API requests to Node server during development

--- example/vite.config.js
+++ example/vite.config.js
@@ -2,4 +2,10 @@ const viteSSRPlugin = require('vite-ssr/plugin')
 
 module.exports = {
   plugins: [viteSSRPlugin],
+  proxy: {
+    '/api': {
+      // This is the server in `node-site` directory
+      target: 'http://localhost:8080',
+    },
+  },
 }
