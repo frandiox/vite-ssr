b63536ac2a16ed2a98b800ade72af1baa7aff7c7
Fran Dios <frankdiox@gmail.com>
2020-11-08 21:17:43 +0900

    Add merge-options for build

--- core/build.js
+++ core/build.js
@@ -1,37 +1,46 @@
 const { ssrBuild, build } = require('vite')
 const replace = require('@rollup/plugin-replace')
 const path = require('path')
+const mergeOptions = require('merge-options').bind({ concatArrays: true })
 const config = require('./plugin')
 
 const [name] = Object.keys(config.alias)
 const input = path.resolve(process.cwd(), 'src/main')
 
 module.exports = async ({ clientOptions = {}, ssrOptions = {} } = {}) => {
-  const clientResult = await build({
-    outDir: path.resolve(process.cwd(), 'dist/client'),
-    rollupInputOptions: { input },
-    alias: config.alias,
-    ...clientOptions,
-  })
+  const clientResult = await build(
+    mergeOptions(
+      {
+        outDir: path.resolve(process.cwd(), 'dist/client'),
+        rollupInputOptions: { input },
+        alias: config.alias,
+      },
+      clientOptions
+    )
+  )
 
-  await ssrBuild({
-    outDir: path.resolve(process.cwd(), 'dist/ssr'),
-    alias: {
-      [name]: path.resolve(__dirname, 'entry-server'),
-    },
-    rollupInputOptions: {
-      ...config.rollupInputOptions,
-      input,
-      plugins: [
-        replace({
-          __VITE_SSR_HTML__: clientResult[0].html.replace(
-            '<div id="app">',
-            '<div id="app" data-server-rendered="true">${html}'
-          ),
-        }),
-      ],
-    },
-    rollupOutputOptions: config.rollupOutputOptions,
-    ...ssrOptions,
-  })
+  await ssrBuild(
+    mergeOptions(
+      {
+        outDir: path.resolve(process.cwd(), 'dist/ssr'),
+        alias: {
+          [name]: path.resolve(__dirname, 'entry-server'),
+        },
+        rollupInputOptions: {
+          ...config.rollupInputOptions,
+          input,
+          plugins: [
+            replace({
+              __VITE_SSR_HTML__: clientResult[0].html.replace(
+                '<div id="app">',
+                '<div id="app" data-server-rendered="true">${html}'
+              ),
+            }),
+          ],
+        },
+        rollupOutputOptions: config.rollupOutputOptions,
+      },
+      ssrOptions
+    )
+  )
 }
