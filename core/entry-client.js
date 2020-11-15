737014112cfba213bf2dc720e0dad56438dc1150
Fran Dios <frankdiox@gmail.com>
2020-11-15 23:48:32 +0900

    Add initialState to first view in library

--- core/entry-client.js
+++ core/entry-client.js
@@ -10,6 +10,19 @@ export default async function (App, { routes }, hook) {
   const app = createSSRApp(App)
   app.use(router)
 
+  let entryRouteName
+  let isFirstRoute = true
+  router.beforeEach((to, from, next) => {
+    if (isFirstRoute || (entryRouteName && entryRouteName === to.name)) {
+      // The first route is rendered in the server and its state is provided globally.
+      isFirstRoute = false
+      entryRouteName = to.name
+      to.meta.state = window.__INITIAL_STATE__ || {}
+    }
+
+    next()
+  })
+
   if (hook) {
     await hook({ app, router, isClient: true, baseUrl: '' })
   }
--- example/src/main.js
+++ example/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import routes from './routes'
 import viteSSR from 'vite-ssr'
 
+// Use route.meta.state as props
 routes.forEach((route) => {
   route.props = (r) => ({ ...(r.meta.state || {}), ...(r.props || {}) })
 })
@@ -11,23 +12,18 @@ export default viteSSR(
   App,
   { routes },
   ({ app, router, isClient, request }) => {
-    // The 'request' is the original server request
-    // and should be used to pass auth/headers to the getProps endpoint
-
-    let isFirstRoute = true
+    // The 'request' is the original server request (undefined in browser)
 
     router.beforeEach(async (to, from, next) => {
-      if (isClient && isFirstRoute && window.__INITIAL_STATE__) {
-        // Do not get props for the first route since it is
-        // already rendered in the server. Instead, use the inital state.
-        isFirstRoute = false
-        to.meta.state = window.__INITIAL_STATE__ || {}
+      if (to.meta.state) {
+        // This route has state already (from server) so it can be reused.
         return next()
       }
 
       const baseUrl = isClient ? '' : new URL(request.url).origin
 
       try {
+        // Get our page props from our custom API:
         const res = await fetch(
           `${baseUrl}/api/getProps?path=${encodeURIComponent(to.path)}&name=${
             to.name
