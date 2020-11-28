e5abbc6da5ac357228bde2414ddb8582f7233f58
Fran Dios <frankdiox@gmail.com>
2020-11-29 00:07:45 +0900

    Generate package.json in SSR build

--- core/build.js
+++ core/build.js
@@ -1,5 +1,6 @@
 const { ssrBuild, build } = require('vite')
 const replace = require('@rollup/plugin-replace')
+const fs = require('fs').promises
 const path = require('path')
 const mergeOptions = require('merge-options').bind({ concatArrays: true })
 const plugin = require('./plugin')
@@ -18,16 +19,20 @@ module.exports = async ({ clientOptions = {}, ssrOptions = {} } = {}) => {
     )
   )
 
+  const ssrOutDirPath = path.resolve(process.cwd(), 'dist/ssr')
+  const entryPoint = await getEntryPoint()
+
   await ssrBuild(
     mergeOptions(
       {
-        outDir: path.resolve(process.cwd(), 'dist/ssr'),
+        outDir: ssrOutDirPath,
+        assetsDir: '',
         alias: {
           [name]: plugin.alias[name].replace('entry-client', 'entry-server'),
         },
         rollupInputOptions: {
           preserveEntrySignatures: 'strict',
-          input: await getEntryPoint(),
+          input: entryPoint,
           plugins: [
             replace({
               __VITE_SSR_HTML__: clientResult[0].html.replace(
@@ -40,5 +45,15 @@ module.exports = async ({ clientOptions = {}, ssrOptions = {} } = {}) => {
       },
       ssrOptions
     )
+  )
+
+  const type =
+    (ssrOptions.rollupOutputOptions || {}).format === 'es'
+      ? 'module'
+      : 'commonjs'
+
+  await fs.writeFile(
+    path.join(ssrOutDirPath, 'package.json'),
+    `{ "type": "${type}", "main": "${path.parse(entryPoint).name + '.js'}" }`
   )
 }
