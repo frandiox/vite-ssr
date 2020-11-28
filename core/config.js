0dbb0450871a60f153d693f4e9f82952b712e7ff
Fran Dios <frankdiox@gmail.com>
2020-11-28 23:57:57 +0900

    Get project info synchronously

--- core/config.js
+++ core/config.js
@@ -1,12 +1,12 @@
-const fs = require('fs').promises
+const fs = require('fs')
 const path = require('path')
 
 const viteConfigJS = 'vite.config.js'
 const viteConfigTS = 'vite.config.ts'
 const systemRoot = path.parse(process.cwd()).root
-const fileExists = async (dir, file) => {
+const fileExists = (dir, file) => {
   try {
-    await fs.access(path.resolve(dir, file))
+    fs.accessSync(path.resolve(dir, file))
     return true
   } catch (_) {
     return false
@@ -16,13 +16,13 @@ const fileExists = async (dir, file) => {
 let rootDir
 let isTS = false
 
-exports.getProjectInfo = async function () {
+exports.getProjectInfo = function () {
   if (!rootDir) {
     let currentDir = process.cwd()
     while (!rootDir && currentDir !== systemRoot) {
-      if (await fileExists(currentDir, viteConfigJS)) {
+      if (fileExists(currentDir, viteConfigJS)) {
         rootDir = currentDir
-      } else if (await fileExists(currentDir, viteConfigTS)) {
+      } else if (fileExists(currentDir, viteConfigTS)) {
         isTS = true
         rootDir = currentDir
       } else {
@@ -43,9 +43,9 @@ exports.getProjectInfo = async function () {
 }
 
 exports.getEntryPoint = async function () {
-  const { rootDir } = await exports.getProjectInfo()
+  const { rootDir } = exports.getProjectInfo()
 
-  const indexHtml = await fs.readFile(
+  const indexHtml = await fs.promises.readFile(
     path.resolve(rootDir, 'index.html'),
     'utf-8'
   )
