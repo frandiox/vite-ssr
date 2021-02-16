de1ba60a88702ee8335af540168cc7f3c273d0bb
Fran Dios <frankdiox@gmail.com>
2021-02-16 21:04:19 +0900

    types: Add server renderer types

--- core/entry-server.d.ts
+++ core/entry-server.d.ts
@@ -2,6 +2,13 @@ declare module 'vite-ssr/entry-server' {
   import Vue, { App } from 'vue'
   import { RouteLocationNormalized, RouteLocationRaw, Router } from 'vue-router'
 
+  type Renderer = (params: {
+    request: Request
+    manifest?: Record<string, string[]>
+    preload?: boolean
+    [key: string]: any
+  }) => Promise<{ html: string; dependencies: string[] }>
+
   const handlerSSR: (
     App: typeof Vue,
     options: {
@@ -16,7 +23,7 @@ declare module 'vite-ssr/entry-server' {
       initialRoute: RouteLocationNormalized
       [key: string]: any
     }) => Promise<void>
-  ) => Promise<() => Promise<{ html: string }>>
+  ) => Promise<Renderer>
 
   export default handlerSSR
 }
