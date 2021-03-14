7e3cf11da528a949ae53a3673547f5c639131e20
Fran Dios <frankdiox@gmail.com>
2021-03-14 23:08:00 +0900

    feat: Support nested routes in React

--- src/react/utils.js
+++ src/react/utils.js
@@ -2,42 +2,49 @@ import React from 'react'
 
 export function createRouter({ routes, initialState, PropsProvider }) {
   let currentRoute = null
+
+  function augmentRoute(originalRoute) {
+    const meta = {
+      ...(originalRoute.meta || {}),
+      state: null,
+    }
+
+    const augmentedRoute = {
+      ...originalRoute,
+      meta,
+      component: (props) => {
+        const from = currentRoute
+        const to = augmentedRoute
+
+        if (!import.meta.env.SSR && !currentRoute) {
+          // First route in browser
+          meta.state = initialState
+        }
+
+        currentRoute = augmentedRoute
+
+        if (PropsProvider) {
+          return React.createElement(
+            PropsProvider,
+            { ...props, from, to },
+            originalRoute.component
+          )
+        }
+
+        return React.createElement(originalRoute.component, props)
+      },
+    }
+
+    if (Array.isArray(originalRoute.routes)) {
+      augmentedRoute.routes = originalRoute.routes.map(augmentRoute)
+    }
+
+    return augmentedRoute
+  }
+
   return {
     getCurrentRoute: () => currentRoute,
-    isFirstRoute: () => isFirstRoute,
-    routes: routes.map((originalRoute) => {
-      const meta = {
-        ...(originalRoute.meta || {}),
-        state: null,
-      }
-
-      const augmentedRoute = {
-        ...originalRoute,
-        meta,
-        component: (props) => {
-          const from = currentRoute
-          const to = augmentedRoute
-
-          if (!import.meta.env.SSR && !currentRoute) {
-            // First route in browser
-            meta.state = initialState
-          }
-
-          currentRoute = augmentedRoute
-
-          if (PropsProvider) {
-            return React.createElement(
-              PropsProvider,
-              { ...props, from, to },
-              originalRoute.component
-            )
-          }
-
-          return React.createElement(originalRoute.component, props)
-        },
-      }
-
-      return augmentedRoute
-    }),
+    isFirstRoute: () => !currentRoute,
+    routes: routes.map(augmentRoute),
   }
 }
