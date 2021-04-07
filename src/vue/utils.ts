2e98d06d0ef0f483a0d734dde1102f7d21ea73c6
Fran Dios <frankdiox@gmail.com>
2021-04-08 01:13:43 +0900

    vue:feat Internal API for HMR

--- src/vue/utils.ts
+++ src/vue/utils.ts
@@ -3,6 +3,7 @@ import type { ExtendedRouteRaw, ExtendedRouteNormalized } from './types'
 export function addPagePropsGetterToRoutes(routes: ExtendedRouteRaw[]) {
   routes.forEach((route) => {
     route.props = (r: ExtendedRouteNormalized) => ({
+      ...(r.meta.hmr || {}).value, // Internal API to refresh page props
       ...(r.meta.state || {}),
       ...((r.props === true ? r.params : r.props) || {}),
     })
