e0fc985e64c264acae1b49b4f537fe0860465034
Cameron Lofgren <Cameron.Lofgren@wbmason.com>
2023-07-14 17:07:59 -0400

    wip: globals should be consts

--- src/react/global.d.ts
+++ src/react/global.d.ts
@@ -1,7 +1,7 @@
 declare global {
-  let __DEV__: boolean
-  let __CONTAINER_ID__: string
-  let __USE_APOLLO_RENDERER__: boolean
+  const __DEV__: boolean
+  const __CONTAINER_ID__: string
+  const __USE_APOLLO_RENDERER__: boolean
 }
 
 export default global
